3a637556740ba0c368c25a9c112958300105a5b8
Panashe Mahachi <pkmahach@uwaterloo.ca>
2015-05-10 06:18:54 -0400

    Changed read_stream permision to user_status

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, FACEBOOK_CONFIG['app_id'], FACEBOOK_CONFIG['secret'], {:scope => 'email,read_stream,user_friends,user_likes'}
+  provider :facebook, FACEBOOK_CONFIG['app_id'], FACEBOOK_CONFIG['secret'], {:scope => 'email,user_status,user_friends,user_likes'}
 end
